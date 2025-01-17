26d00f13421c6aeb9ecb32034e1a2764a92b40a3
Dark-Robin <sihileld23@gmail.com>
2025-01-17 16:48:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ const express = require('express');
 const ytdl = require('ytdl-core');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('YouTube Downloader API is running.');
+  res.send('YouTube Audio Downloader API is running!');
 });
 
-// API endpoint to generate MP3 download link
 app.get('/download/ytmp3', async (req, res) => {
   try {
     const videoUrl = req.query.url;
@@ -17,17 +15,16 @@ app.get('/download/ytmp3', async (req, res) => {
     }
 
     const info = await ytdl.getInfo(videoUrl);
-    const title = info.videoDetails.title;
-
-    // Get audio format
     const audioFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
-    const downloadLink = audioFormat.url;
 
     res.json({
-      title,
-      downloadLink,
+      title: info.videoDetails.title,
+      downloadLink: audioFormat.url,
     });
   } catch (error) {
     console.error(error);
-   
+    res.status(500).json({ error: 'Failed to process the request.' });
+  }
+});
 
+module.exports = app;
